6d1b9b67cc3a6cc41f5d3a6ecfd2d66b3f65c72e
Caio Alexandre <contato.kyomi@gmail.com>
2021-10-08 18:29:38 -0300

    Bump to v0.1.4

--- qualitube/__init__.py
+++ qualitube/__init__.py
@@ -5,7 +5,7 @@ __title__ = 'qualitube'
 __author__ = 'Vitor Mussa'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2021-present Vitor Mussa'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 
 from .client import Client as Client
